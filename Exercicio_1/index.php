aa5031e04c7ee2a7ec7d448fabf44f651dd2462c
ganjos <gmmanjos@gmail.com>
2024-10-16 11:55:58 -0300

    Exercicio_1 - Desenvolvendo padrão da estrutura front-end utilizando bootstrap

---
+++ Exercicio_1/index.php
@@ -0,0 +1,25 @@
+<!-- 
+Exercício 1
+Crie uma página em HTML e PHP/JAVASCRIPT que receba uma
+palavra e que remova todas as vogais desta palavra, como no
+exemplo abaixo
+PS.: Nenhuma palavra com acentos e caracteres especiais será
+testada: 
+
+-->
+
+<?php include '../public/partials/header.php'; ?>
+
+<div class="container mt-5">
+    <h1 class="text-center">Remover Vogais</h1>
+    <form method="post" class="mt-4">
+        <div class="form-group">
+            <input type="text" name="word" class="form-control" placeholder="Digite uma palavra" required>
+        </div>
+        <button type="submit" class="btn btn-primary btn-block">Remover Vogais</button>
+    </form>
+
+
+</div>
+
+<?php include '../public/partials/footer.php'; ?>
